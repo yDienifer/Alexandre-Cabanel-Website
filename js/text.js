c7e98eccb094181fc38d5edfa736e5b409276cf2
Diênifer <108842368+yDienifer@users.noreply.github.com>
2022-11-14 15:56:50 -0300

    Add files via upload

---
+++ js/text.js
@@ -0,0 +1,36 @@
+function changeContentFallenAngel(fallenAngel) {
+    document.getElementById('background-arts').src = "img/Fallen-Angel.jpg";
+    document.getElementById('content').textContent = "The Fallen Angel painting depicts Lucifer after being cast out of heaven, when he rebelled against God. The fallen angel hides his face with his right arm, and a tear about to fall is visible in his right eye, in his left eye, there is a stronger shadow, which apparently hides another tear. In their eyes is anger, pride and a desire for revenge. Just above, angels are visualized, commemorating the fall of Lucifer.";
+    document.getElementById('title').style.color = "#99bbff70";
+    document.getElementById('title').textContent = "Fallen Angel";
+}
+
+function changeContentVenus(venus) {
+    document.getElementById('background-arts').src = "img/The Birth of Venus.jpg";
+    document.getElementById('title').textContent = "The Birth of Venus";
+    document.getElementById('title').style.color = "";
+    document.getElementById('content').textContent = "The painting represents the goddess Venus, with her naked body and a shy look, cast under the crook of her elbow. She is lying down, relaxed as well, with a sleepy expression, and her hair conveys the purity and allure of the goddess. As a mythological painting, it passes the seduction, being very flashy, to the eyes that visualize it.";
+}
+
+function changeContentPhaedra(phaedra) {
+    document.getElementById('background-arts').src = "img/Phaedra.jpg";
+    document.getElementById('title').textContent = "Phaedra";
+    document.getElementById('title').style.color = "rgba(221, 51, 51, 0.562)";
+    document.getElementById('content').textContent = "The painting shows Phaedra, with a death glare, lying on a bed, her hair mussed on the pillow, her body naked, beneath delicate white sheets. The bedroom is luxurious, and to your right, you can see two girls. A girl is sleeping, apparently very tired, apparently due to emotional exhaustion. The other woman has her hands clasped together, as if silently begging Phaedra to move. Alexandre recounts, in his painting, the pain of Phaedra, of having been rejected by her great love, and that, at that moment, she wants death.";
+}
+
+function changeContentMoses(moses) {
+    document.getElementById('background-arts').src = "img/The Death of Moses.jpeg";
+    document.getElementById('title').textContent = "The Death of Moses";
+    document.getElementById('title').style.color = "orange";
+    document.getElementById('content').textContent = "The painting represents Moses, dying, before God, before reaching the promised land, seeing it like this, from afar. In a letter to his brother, Alexandre said: 'I have imposed upon myself a large, very difficult, formidable task, since I seek to represent the image of the Eternal Master of the sky and the earth to represent God and next to Him, one of His most sublime creatures, deified in some way by His contact'."
+}
+
+function changeContentEcho(echo) {
+    document.getElementById('background-arts').src = "img/Echo.jpg";
+    document.getElementById('title').textContent = "Echo";
+    document.getElementById('title').style.color = "brown";
+    document.getElementById('content').textContent = "Being a mythological painting, it recounts the curse of Echo, who can only utter his last spoken words. In the painting, it shows Echo screaming, with her hands over her ears, apparently scared, due to some sound. As a very elegant painting, it was praised by many judges, although some judged the art to be unconvincing because of Echo's nudity. Viewers, on the other hand, found the art very realistic, but obscene.";
+}
+
+// Menu hamburger
